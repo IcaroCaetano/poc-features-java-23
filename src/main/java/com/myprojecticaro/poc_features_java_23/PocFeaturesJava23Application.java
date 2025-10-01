a55fd25e9c546905bcce1713e2636c153c9e92df
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-10-01 16:27:21 -0300

    Update PocFeaturesJava23Application.java

--- src/main/java/com/myprojecticaro/poc_features_java_23/PocFeaturesJava23Application.java
+++ src/main/java/com/myprojecticaro/poc_features_java_23/PocFeaturesJava23Application.java
@@ -1,13 +1,21 @@
 package com.myprojecticaro.poc_features_java_23;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.myprojecticaro.poc_features_java_23.features.*;
 
-@SpringBootApplication
 public class PocFeaturesJava23Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PocFeaturesJava23Application.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("🚀 Running Java 23 Features POC");
 
+        // Call each feature demo for java 23
+        StringTemplates.runExample();
+        UnnamedPatterns.run("Hello");
+        UnnamedPatterns.run(42);
+        UnnamedVariables.runExample();
+        ScopedValues.runExample();
+        StreamGatherers.run();
+        VectorApi.run();
+
+        System.out.println("✅ Finished running Java 23 features.");
+    }
 }
